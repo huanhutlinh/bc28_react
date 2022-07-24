884901df651df126aaaa2f326036e66ee2465faa
macbook <khaitruong1301@gmail.com>
2022-07-24 11:27:29 +0700

    buoi1_createComponent_databinding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import DemoFunctionCom from './Components/DemoComponent/DemoFunctionCom';
+import DemoClassCom from './Components/DemoComponent/DemoClassCom';
+import BaiTapLayout from './Components/BaiTapLayout/BaiTapLayout';
+import Databinding from './Databinding/Databinding';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <BaiTapLayout /> */}
+        <Databinding />
     </div>
   );
 }
